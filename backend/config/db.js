58446b1d1277cd69ad18592bf433ab701a1e912c
haseeb62 <23100044@lums.edu.pk>
2025-01-06 22:33:33 +0500

    new version changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,18 @@
-import mongose from 'mongoose'
+import mongoose from 'mongoose'
 import colors from 'colors'
 
 // a mongoose stuf (mongoose.connect ....) return always a promise
 const connectDB = async () => {
-    try{
-        const conn = await mongose.connect(process.env.MONGO_URI,{
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true
-        })
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
+            dbName: 'Ecommerce',
+        }); // No additional options required
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
     } catch (error) {
-      console.error(`Error: ${error.message}`.red.underline.bold)
-            process.exit(1)
+        console.error(`Error: ${error.message}`.red.underline.bold);
+        process.exit(1);
     }
-}
+};
+
 
 export default connectDB
